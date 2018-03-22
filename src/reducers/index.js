0e567985117d9127c66dc08c32b5fdcf575b1fcc
Pawan Singh <pawansx.1@gmail.com>
2018-03-22 20:48:58 +0530

    Add ./reducers/index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+// /reducers/index.js
+
+import { combineReducers } from 'redux';
+import contacts from './contactReducer';
+
+export default combineReducers(
+    {
+        contacts: contacts
+    }
+)
