684457d2a5883b771559531642b330c4bfe07091
Pawan Singh <pawansx.1@gmail.com>
2018-03-22 17:28:45 +0530

    Add index.js

--- src/index1.js
+++ src/index1.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import { Provider } from 'react-redux';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
