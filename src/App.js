d6625c973dcbf79183fa463603591e8eea167472
Pawan Singh <pawansx.1@gmail.com>
2018-03-27 21:46:53 +0530

    Add Bootstrap in App.js

--- src/App.js
+++ src/App.js
@@ -33,40 +33,66 @@ class App extends Component {
         console.log(this.state.name);
     }
 
-    render () {
-        let name;
+    listView(data, index) {
         return (
-            <div>
-                <h1>Clientside Contacts App</h1>
-                <hr />
-                
-                { <ul>
-                    {this.props.contacts.map((contact, i) => <li key={i}>{contact.name}</li> )}
-                </ul> }
-
-                <div>
-                    <h3>Add Contact Form</h3>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="text" onChange={this.handleChange} />
-                        <input type="submit" />
-                    </form>
+            <div className='row'>
+                <div className='col-md-10'>
+                    <li key={index} className='list-group-item clearfix'>
+                        {data.name}
+                    </li>
                 </div>
-            
+                <div className='col-md-2'>
+                    <button onClick={(e)=> this.deleteContact(e, index)} className='btn btn-danger'>
+                        Remove
+                    </button>
+
+                </div>
             </div>
         )
     }
-}
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        contacts: state.contact
+    deleteContact(e, index) {
+        e.preventDefault();
+        this.props.deleteContact(index);
     }
-};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        createContact: contact => dispatch(contactAction.createContact(contact))
+    render () {
+        let name;
+        return (
+                <div className='Container'>
+                    <h1>Clientside Contacts App</h1>
+                    <hr />
+                    
+                    <div>
+                        <h3>Add Contact Form</h3>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" onChange={this.handleChange} className='form-control' value={this.state.name} /> <br/>
+                            <input type="submit" 
+                           className='btn btn-success'
+                           value='ADD' />
+                        </form>
+                        <hr />
+
+                        { <ul className="list-group">
+                            {this.props.contacts.map((contact, i) => this.listView(contact, i))}
+                        </ul> }
+                    </div>
+                
+                </div>
+            )
+        }
     }
-};
+
+    const mapStateToProps = (state, ownProps) => {
+        return {
+            contacts: state.contact
+        }
+    };
+
+    const mapDispatchToProps = (dispatch) => {
+        return {
+            createContact: contact => dispatch(contactAction.createContact(contact))
+        }
+    };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
